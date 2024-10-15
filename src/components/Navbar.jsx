720ff855a805ea862eede6fafb1547edcec9a14c
Ashish Kumar <akdev5520@gmail.com>
2024-10-16 00:36:34 +0530

    project setup done

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+function Navbar() {
+	return (
+		<header className="fixed top-0 left-0 right-0 z-50 bg-black/90">
+			<div className="mx-auto max-w-7xl">
+				<div className="flex items-center justify-between py-5 mx-auto c-space">
+					<a href="/" className="text-neutral-400"></a>
+				</div>
+			</div>
+		</header>
+	);
+}
+
+export default Navbar;
